e068ffe6b6ffeaa3255b39a34cb299536df9cd20
ayoung <cvbjiop@gmail.com>
2025-07-25 16:08:16 +0900

    feat: 메인페이지 구현, 라디오페이지와 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
 import './css/App.css';
+import MainPage from './pages/MainPage';
 import Home from './pages/Home';
 import RecommendationPage from './pages/RecommendationPage';
 // import MyPage from './pages/MyPage'; 나중에 구현할 페이지
@@ -8,10 +9,10 @@ const Layout = () => (
   <div className="App">
     <header className="App-header">
       <nav>
-        <Link to="/">Home</Link>
-        {/* <Link to="/my-page">My Page</Link>  미리 연결만해두고 나중에 구현 */ } 
+        <Link to="/">Room</Link>
+        {/* <Link to="/my-page">My Page</Link>  미리 연결만해두고 나중에 구현 */ }
       </nav>
-      <Outlet /> 
+      <Outlet />
     </header>
   </div>
 );
@@ -20,7 +21,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} /> 
+        <Route index element={<MainPage />} />
+        <Route path="selection" element={<Home />} />
         <Route path="recommendation" element={<RecommendationPage />} />
       </Route>
     </Routes>
