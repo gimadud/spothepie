4010c5356722f7289e7e7f663861147c039322fa
ayoung <cvbjiop@gmail.com>
2025-08-01 15:26:53 +0900

    feat: 카세트테이프 내부 모달창 구현

--- src/components/SavedSongsModal.tsx
+++ src/components/SavedSongsModal.tsx
@@ -20,17 +20,42 @@ const moodKorean: { [key in SavedSongsModalProps['mood']]: string } = {
   tiredness: '피곤',
 };
 
+const moodColors: { [key in SavedSongsModalProps['mood']]: string } = {
+  joy: '#F6E381', 
+  sadness: '#87B1DC', 
+  angry: '#F2798F', 
+  relaxed: '#A6D1B5', 
+  happiness: '#F9C1C0', 
+  anxiety: '#F2AD85', 
+  depression: '#BAA8D2', 
+  tiredness: '#BFBFBF',
+};
+
+const moodLightColors: { [key in SavedSongsModalProps['mood']]: string } = {
+  joy: '#FFF5BE', 
+  sadness: '#BCDAF9', 
+  angry: '#F29F9F', 
+  relaxed: '#C5E4CF', 
+  happiness: '#FFDFDF', 
+  anxiety: '#FFCAAC', 
+  depression: '#E7D7FC', 
+  tiredness: '#E9E9E9', 
+};
+
 const SavedSongsModal: React.FC<SavedSongsModalProps> = ({ songs, mood, onClose, onDelete }) => {
+  const backgroundColor = moodColors[mood];
+  const listItemBackgroundColor = moodLightColors[mood];
+
   return (
-    <div className="modal-overlay">
-      <div className="modal-content">
-        <div className='modal-top'>
+    <div className="song-modal-overlay">
+      <div className="song-modal-content" style={{ backgroundColor }}>
+        <div className='song-modal-top'>
           <h2>{moodKorean[mood]} 카세트</h2>
-          <button className="close-button" onClick={onClose}>닫기</button>
+          <button className="song-close-button" onClick={onClose}>닫기</button>
         </div>
         <ul>
           {songs.map((song, index) => (
-            <li key={index}>
+            <li key={index} style={{ backgroundColor: listItemBackgroundColor }}>
               <img src={song.artworkUrl100} alt={song.trackName} />
               <div>
                 <h3>{song.trackName}</h3>
